7fa29e988329f79781d7f123a35b8717755665db
umschaudhary <umesschaudhary@gmail.com>
2022-08-05 00:59:38 +0545

    basic setup app

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import {UsersModule} from 'src/users/users.module'
+
+@Module({
+  imports:[
+    UsersModule
+  ],
+  providers: []
+})
+export class AppModule {}
