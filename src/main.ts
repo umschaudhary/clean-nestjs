28497d7904efe0f991639887ac1d9059b9c7064d
umschaudhary <umesschaudhary@gmail.com>
2022-08-15 01:15:39 +0545

    enable security and cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {AppModule} from "./app/app.module" 
-import { setUpSwagger } from './utils/swagger';
+import { setUpSwagger, setupSecurity } from './utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  setupSecurity(app)
   setUpSwagger(app)
   await app.listen(3000);
 }
--- src/utils/security.ts
+++ src/utils/security.ts
@@ -1,39 +1,39 @@
-// import { INestApplication } from '@nestjs/common'
-// import * as rateLimit from 'express-rate-limit'
-// import * as helmet from 'helmet';
-//
-// const allowedOrigins = [
-//   // WEBSITES
-//   process.env.ALLOWED_DOMAIN,
-//   'http://localhost',
-//   'http://127.0.0.1',
-// ]
-//
-// export function setupSecurity(app: INestApplication): void {
-//   // Set security-related HTTP headers
-//   app.use(helmet())
-//   // Enable Cross-origin resource sharing for a list of domains
-//   app.enableCors({
-//     origin: (origin, callback) => {
-//       if (!origin || allowedOrigins.some((o) => origin.startsWith(o))) {
-//         callback(null, true)
-//       } else {
-//         callback(new Error('Origin not allowed by CORS'))
-//       }
-//     },
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//     credentials: true,
-//   })
-//   // Limit repeated requests for brute-force attacks
-//   app.use(
-//     rateLimit({
-//       windowMs: 5 * 60 * 1000, // 5 minutes
-//       max: 1000, // limit each IP to 1000 requests per windowMs
-//       message:
-//         'Too many requests created from this IP, please try again after 5 minutes',
-//     }),
-//   )
-//   // Mitigate Cross-site request forgery (CSRF or XSRF)
-//   // It requires either session middleware or a cookie-parser to be initialized first
-//   // app.use(csurf())
-// }
+import { INestApplication } from '@nestjs/common'
+import rateLimit from 'express-rate-limit'
+import helmet from 'helmet'
+
+let allowedOrigins = ['*']
+const hosts = process.env.ALLOWED_HOSTS
+if (hosts) {
+  allowedOrigins = allowedOrigins.concat(hosts.split(','))
+}
+
+const setupSecurity = (app: INestApplication) => {
+  // Set security-related HTTP headers
+  app.use(helmet())
+  // Enable Cross-origin resource sharing for a list of domains
+  app.enableCors({
+    origin: (origin, callback) => {
+      console.log(origin)
+      console.log(allowedOrigins)
+      if (!origin || allowedOrigins.some((o) => origin.startsWith(o))) {
+        callback(null, true)
+      } else {
+        callback(new Error('Origin not allowed by CORS'))
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
+  // Limit repeated requests for brute-force attacks
+  app.use(
+    rateLimit({
+      windowMs: 5 * 60 * 1000, // 5 minutes
+      max: 1000, // limit each IP to 1000 requests per windowMs
+      message:
+        'Too many requests created from this IP, please try again after 5 minutes',
+    }),
+  )
+}
+
+export { setupSecurity }
